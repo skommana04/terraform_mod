67d1deb0d8a86007aec6fee4289b8a16a96f13c5
skommana04 <skommana04@gmail.com>
2025-01-21 21:42:06 -0600

    1

--- Development/main.tf
+++ Development/main.tf
@@ -1,8 +1,7 @@
 module "vpc" {
   source            = "../modules/network"
   cidr_block        = var.cidr_block
-  vpc_id            = module.network.vpc_id
-  count             = length(var.public_cidr_block)
+   count             = length(var.public_cidr_block)
   public_cidr_block = element(var.public_cidr_block, count.index)
   availability_zone = element(var.availability_zone, count.index)
 
--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -3,9 +3,21 @@ resource "aws_vpc" "VPC_medha" {
   enable_dns_hostnames = true
   instance_tenancy = "default"
  
+    tags = {
+    Name = "VPC_Medha"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  count = length(var.public_cidr_block)
+  vpc_id     = aws_vpc.VPC_medha.id
+  cidr_block = var.public_cidr_block
+  availability_zone = element(var.availability_zone, count.index)
+
+#   public_cidr_block = "10.0.1.0/24"
   
 
   tags = {
-    Name = "VPC_Medha"
+    Name = "Public_Subnet_${count.index + 1}"
   }
 }
