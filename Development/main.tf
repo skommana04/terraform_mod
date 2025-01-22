e5818e472b61c92285dec77b09ef01f896d427c5
skommana04 <skommana04@gmail.com>
2025-01-21 19:59:10 -0600

    up2

--- Development/main.tf
+++ Development/main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source            = "../modules/network"
   cidr_block        = var.cidr_block
-  vpc_id            = module.vpc.vpc_id
+  vpc_id            = module.network.vpc_id
   count             = length(var.public_cidr_block)
   public_cidr_block = element(var.public_cidr_block, count.index)
   availability_zone = element(var.availability_zone, count.index)
